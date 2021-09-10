4ec6833637607433d4269b4d167ff2452ba68a68
Parth Dalal <parthdalal06@gmail.com>
2021-09-10 14:04:16 +0530

    code restructuring

--- src/pages/Authentication/Authentication.jsx
+++ src/pages/Authentication/Authentication.jsx
@@ -1,31 +1,34 @@
-import React from "react";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import ReactPhoneInput from "react-phone-input-material-ui";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import { useState } from "react";
-import { useRef } from "react";
-import { useHistory } from "react-router-dom";
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import ReactPhoneInput from 'react-phone-input-material-ui';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { useState } from 'react';
+import { useRef } from 'react';
+import { useHistory } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import  {companyApiProvider} from 'services/api/company/companyService';
+import  {userApiProvider}  from 'services/api/user/userService';
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
+      {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
         TCAP
-      </Link>{" "}
+      </Link>{' '}
       {new Date().getFullYear()}
-      {"."}
+      {'.'}
     </Typography>
   );
 }
@@ -33,16 +36,16 @@ function Copyright() {
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: "100%", // Fix IE 11 issue.
+    width: '100%', // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -50,62 +53,34 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Authentication() {
+export default function Authentication({ setToken }) {
   let history = useHistory();
   const classes = useStyles();
-  const [phone, setPhone] = useState("");
+  const [phone, setPhone] = useState('');
   const [verifyOtpButton, setVerifyOtpButton] = useState(false);
-  const otpRef = useRef();
+  const [otp,setOtp] = useState('');
+  const [email,setEmail] = useState('');
 
-  const getVerificationOtp = async () => {
+  const getVerificationOtp = async (event) => {
+    console.log(event);
     let formData = {
-      email: "parth@trustless.capital",
-      phoneNumber: "+918962562924",
+      email: email,
+      phoneNumber: `+${phone}`,
     };
-    // formData.append("email", "test@trustless.capital");
-    // formData.append("phoneNumber", "+911234567890");
-    const response = await fetch("http://18.118.12.136/api/user/signup", {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    })
-      .then((response) => {
-        console.log("signup success response:" + response);
-        fetch("http://18.118.12.136/api/user/resendOTP", {
-          method: "post",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(formData),
-        });
-      })
-      .catch((error) => {
-        console.log("signup failed response" + error);
-      });
-    console.log(response);
+    const response = await userApiProvider.signUp(formData);
+    console.log("signup response",response.status);
+    const otpReq = await companyApiProvider.resendOTP({email:formData.email});
+    console.log("resend otp",otpReq);
     setVerifyOtpButton(true);
   };
   const verifyOTP = async () => {
-    let formData = new FormData();
-    const response = await fetch(
-      "http://18.118.12.136/api/company/signup/verifyOTP",
-      {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: "parth@trustless.capital",
-          otp: otpRef.current.value,
-        }),
-      }
-    ).catch((error) => {
-      console.log("verifyotp failed response" + error);
-    });
-    console.log("verifyotp success response" + response);
-    history.push("/");
+    let formData = {
+      email: email,
+           otp: otp,
+    }
+    const response = companyApiProvider.verifyOTP(formData);
+    console.log("verifyOTP",response);
+    setToken({user:"investor1",type:"investor"});
   };
   return (
     <Container component="main" maxWidth="xs">
@@ -128,11 +103,13 @@ export default function Authentication() {
             name="email"
             autoComplete="email"
             autoFocus
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
           />
           <ReactPhoneInput
-            defaultCountry={"us"}
+            defaultCountry={'us'}
             value={phone}
-            onChange={() => setPhone(phone)}
+            onChange={value => setPhone(value)}
             inputClass={classes.field}
             dropdownClass={classes.countryList}
             component={TextField}
@@ -147,7 +124,7 @@ export default function Authentication() {
               label="OTP"
               type="number"
               id="otp"
-              ref={otpRef}
+              onChange={(event)=>setOtp(event.target.value)}
             />
           )}
           {/* <FormControlLabel
@@ -196,3 +173,6 @@ export default function Authentication() {
     </Container>
   );
 }
+Authentication.propTypes = {
+  setToken: PropTypes.func.isRequired,
+};
---
+++ src/stub/stub.js
@@ -0,0 +1,27 @@
+const getverificationOtpStub = {
+    email: 'parth@trustless.capital',
+    phoneNumber: '+918962562924',
+  };
+
+  const verifyOtpStub = {
+    email: 'parth@trustless.capital',
+    otp: '2203',
+  }
+  const companyStub = {
+    email: 'hello@trustless.com',
+    phoneNumber: '+918600936299',
+    country: 'IND',
+    state: 'TELANGANA',
+    city: 'HYDERABAD',
+    address: 'Fianancial District',
+    adminName: 'Raj',
+    companyId: 'TN-LABS',
+    organisationName: 'TELANGANA ORG',
+    type: 'vendor',
+  };
+
+  export const stub = {
+    getverificationOtpStub,
+    verifyOtpStub,
+    companyStub
+  }
