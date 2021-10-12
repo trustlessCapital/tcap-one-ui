92a3b97ff421e80e05f9f8037bf72d097d45e55b
Parth Dalal <parthdalal06@gmail.com>
2021-10-13 03:26:43 +0530

    new pages inclusion

---
+++ src/components/InvoiceList/invoicelist.jsx
@@ -0,0 +1,140 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+
+const columns = [
+  { id: 'code', label: 'Buyer ID', minWidth: 100 },
+  { id: 'name', label: 'Company Name', minWidth: 170 },
+  {
+    id: 'population',
+    label: 'Type of Company',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'GST',
+    label: 'GST Number',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'Relationship',
+    label: 'Relationship with TCAP',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toFixed(2),
+  },
+  {
+    id: 'ApprovedLimit',
+    label: 'Approved Limit',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toFixed(2),
+  },
+];
+
+function createData(name, code, population, GST,Relationship,ApprovedLimit) {
+  return { name, code, population, GST, Relationship,ApprovedLimit };
+}
+
+const rows = [
+  createData('Wipro', '123','Company','u12hg3uh21uyg3u', 'Buyer', 3287263),
+  createData('Tcs', '234','Company','u12hg3uh21uyg3u', 'Buyer', 9596961),
+  createData('Infosys', '235','Company', 'u12hg3uh21uyg3u','Buyer', 301340),
+  createData('Tcs States', '1245','Company','u12hg3uh21uyg3u', 'Buyer', 9833520),
+  createData('Tcs', '73','Company', 'u12hg3uh21uyg3u','Buyer', 9984670),
+  createData('Infosys', '2','Company', 'u12hg3uh21uyg3u','Buyer', 7692024),
+  createData('Wipro', '5342','Company','u12hg3uh21uyg3u', 'Buyer', 357578),
+  createData('Wipro', '423','Partnerhip LLP','u12hg3uh21uyg3u', 'Buyer', 70273),
+  createData('Tcs', '12','Company', 'u12hg3uh21uyg3u','Buyer', 1972550),
+  createData('Infosys', '34','Company', 'u12hg3uh21uyg3u','Buyer', 377973),
+  createData('France', '76','Company', 'u12hg3uh21uyg3u','Seller', 640679),
+  createData('Infosys ', '765','Company', 'u12hg3uh21uyg3u','Seller', 242495),
+  createData('Infosys', '8','Company', 'u12hg3uh21uyg3u','Seller', 17098246),
+  createData('Wipro', '8765','Company', 'u12hg3uh21uyg3u','Buyer/Seller', 923768),
+  createData('Wipro', '234','Company', 'u12hg3uh21uyg3u','Buyer/Seller', 8515767),
+];
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+  container: {
+    maxHeight: 440,
+  },
+});
+
+export default function InvoiceList() {
+  const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper className={classes.root}>
+      <TableContainer className={classes.container}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
---
+++ src/pages/arranger/arranger.jsx
@@ -0,0 +1,171 @@
+//import LgWidgets from '../../components/lgwidgets/LgWidgets'
+//import SmWidgets from '../../components/smwidgets/SmWidgets'
+//import AiWidgets from '../components/aiWidgets/AiWidgets'
+import './arranger.css';
+//import Select from 'react-select';
+import React, { Component, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Publish } from '@material-ui/icons';
+// import { Component, useState } from "react";
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import EntityList from 'components/EntityList/entityList';
+import InvoiceList from 'components/InvoiceList/invoicelist';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    flexGrow: 1,
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  buttonMargin: {
+    margin: theme.spacing(2),
+  },
+}));
+
+export default function Arranger() {
+  // userData;
+  const classes = useStyles();
+
+  return (
+    <div className="addInvPage">
+      <h3 className="addInvPageTitle">Admin</h3>
+      <Button variant="contained">
+        <Link to="/onboardentity">Onboard Buyer/Seller</Link>
+      </Button>
+      <Button variant="contained">
+        <Link to="/addinvoices">Upload Invoices</Link>
+      </Button>
+      <Button variant="contained">
+        <Link to="/viewinvoices">View Invoices</Link>
+      </Button>
+      {/* <Grid container xs={12}>
+        <Grid item xs={10}>
+          <FormControlLabel
+            control={
+              <Switch
+                checked={state.viewOnly}
+                onChange={handleViewOnly}
+                name="viewOnly"
+                color="primary"
+              />
+            }
+            label="View Only"
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <Button variant="contained" className={classes.buttonMargin}>
+            SAVE
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.buttonMargin}
+          >
+            SAVE AND CLOSE
+          </Button>
+        </Grid>
+      </Grid> */}
+      <div className="addInvPageWrapper">
+        {/* <Dialog
+          open={state.open}
+          onClose={onHandleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">{'Submit Invoice?'}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Are you sure you want to submit the invoice?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={onHandleClose} color="primary">
+              No
+            </Button>
+            <Button onClick={onHandleClose} color="primary" autoFocus>
+              Yes
+            </Button>
+          </DialogActions>
+        </Dialog> */}
+        <div className={classes.root}>
+          <Accordion color="primary">
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="entity-details"
+              id="entity-details"
+            >
+              <Typography className={classes.heading}>INVOICES</Typography>
+              {/* <Button variant="contained"><Link to="/onboardentity">ADD NEW</Link></Button> */}
+              {/* <Button variant="contained"> LOOKUP</Button> */}
+            </AccordionSummary>
+            <AccordionDetails>
+              <div className={classes.root}>
+                <Grid container spacing={3}>
+                  <Grid item xs={12}>
+                    <InvoiceList />
+                  </Grid>
+                </Grid>
+              </div>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="entity-list"
+              id="entity-list"
+            >
+              <Typography className={classes.heading}>
+                BUYERS/SELLERS
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <div className={classes.root}>
+                <Grid container spacing={3}>
+                  <Grid item xs={12}>
+                    <EntityList />
+                  </Grid>
+                </Grid>
+              </div>
+            </AccordionDetails>
+          </Accordion>
+        </div>
+      </div>
+    </div>
+  );
+}
