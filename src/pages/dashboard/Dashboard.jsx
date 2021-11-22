ff90e30cb2c1304bfb50986dfee4d3df27a0b0cc
Parth Dalal <parthdalal06@gmail.com>
2021-11-22 15:48:26 +0530

    latest updates

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -8,10 +8,10 @@ export default function Dashboard(props) {
     const userData = JSON.parse(localStorage.getItem('userData'));
     return (
         <div className="home">
-            {userData.type!='investor'&& <Home verified = {props?.verified} userData = {props?.userData}/>}
+            {userData.userType!='investor' && <Home verified = {props?.verified} userData = {props?.userData}/>}
             <br></br>
            {(props?.verified?.tcapRelation=='admin' || props?.verified?.tcapRelation=='arranger' || props.userData?.user=='hello@trustless.capital') && <AdminInvoices verified = {props?.verified} userData = {props?.userData}/>}
-           {userData.type=='investor' && <Investor verified = {props?.verified} userData = {props?.userData}/>}
+           {userData.userType=='investor' && <Investor verified = {props?.verified} userData = {props?.userData}/>}
         </div>
     )
 }
