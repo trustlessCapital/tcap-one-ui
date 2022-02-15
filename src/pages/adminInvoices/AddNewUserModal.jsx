0e24e2f9fd84f1291b43ff4c2646b3d815bc28b4
v3n0m <rahul004@protonmail.ch>
2022-02-15 17:37:09 +0530

    New user adding functionality done

--- src/pages/adminInvoices/AddNewUserModal.jsx
+++ src/pages/adminInvoices/AddNewUserModal.jsx
@@ -1,152 +1,184 @@
-import {React, useState, useEffect} from "react";
+import { React, useState, useEffect } from "react";
 import Modal from "react-bootstrap/Modal";
+import { companyApiProvider } from "services/api/company/companyService";
+
 import "./admin.css";
 import Button from "react-bootstrap/Button";
 import {
-    MDBValidation,
-    MDBInput,
-    MDBBtn,
-    MDBCheckbox,
-    MDBRadio
-  } from 'mdb-react-ui-kit';
-  import Row from "react-bootstrap/Row";
-  import Container from "react-bootstrap/Container";
-  import Col from "react-bootstrap/Col";
-
-
+  MDBValidation,
+  MDBInput,
+  MDBBtn,
+  MDBCheckbox,
+  MDBRadio,
+} from "mdb-react-ui-kit";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
 
 export default function AddNewUserModal(props) {
+  const [formValue, setFormValue] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    mob: "",
+    company: "",
+    inlineRadio: "",
+    UserType: "",
+  });
 
-    const [formValue, setFormValue] = useState({
-        fname: 'Mark',
-        lname: 'Otto',
-        email: '',
-        mob: '',
-        company: '',
-        inlineRadio: '',
-        UserType: '',
-      });
-   
+  const handleChange = (e) => {
+    const newData = { ...formValue };
+    newData[e.target.id] = e.target.value;
+    setFormValue(newData);
+  };
 
-    
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    const onChange = (e: any) => {
-      setFormValue({ ...formValue, [e.target.name]: e.target.value });
-    }
-    
-    return (
-      <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Add New User
-          </Modal.Title>
-        </Modal.Header>
-        <MDBValidation className='row g-3' noValidate>
+    companyApiProvider.addNewUser({
+      firstName: formValue.fname,
+      lastName: formValue.lname,
+      email: formValue.email,
+      phoneNumber: "+91" + formValue.mob,
+      walletAddress: "NA",
+      userType: formValue.UserType,
+    });
+  };
+
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered>
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Add New User
+        </Modal.Title>
+      </Modal.Header>
+      {/* <MDBValidation className="row g-3"> */}
+      <form onSubmit={(e) => handleSubmit(e)}>
         <Modal.Body>
-            
-            <Container fluid>
+          <Container fluid>
             <Row className="mt-4">
               <Col md={6}>
                 <MDBInput
                   value={formValue.fname}
-                  name='fname'
-                  onChange={onChange}
-                  id='validationCustom01'
-                  label='First Name'
+                  name="fname"
+                  onChange={(e) => handleChange(e)}
+                  id="fname"
+                  label="First Name"
                 />
               </Col>
               <Col md={6}>
                 <MDBInput
                   value={formValue.lname}
-                  name='lname'
-                  onChange={onChange}
-                  id='validationCustom02'
-                  label='Last name'
+                  name="lname"
+                  onChange={(e) => handleChange(e)}
+                  id="lname"
+                  type="text"
+                  label="Last name"
                 />
               </Col>
             </Row>
             <Row className="mt-4">
-                <Col md={1}>
-                  <span className='input-group-text' id='inputGroupPrepend'>
-                    @
-                  </span>
-                </Col>
-                <Col md={10}>
-                  <MDBInput
+              <Col md={1}>
+                <span className="input-group-text" id="inputGroupPrepend">
+                  @
+                </span>
+              </Col>
+              <Col md={10}>
+                <MDBInput
                   value={formValue.email}
-                  name='email'
-                  onChange={onChange}
-                  id='validationCustom04'
+                  name="email"
+                  onChange={(e) => handleChange(e)}
+                  id="email"
                   required
-                  validation='Please provide an Email'
+                  type="email"
+                  validation="Please provide an Email"
                   invalid
-                  label='Email*' 
+                  label="Email*"
                 />
-                </Col>
+              </Col>
             </Row>
             <Row className="mt-4">
               <Col md={6}>
                 <MDBInput
                   value={formValue.mob}
-                  name='mob'
-                  onChange={onChange}
-                  id='validationCustom03'
+                  name="mob"
+                  type="number"
+                  onChange={(e) => handleChange(e)}
+                  id="mob"
                   required
-                  label='Mobile Number*'
-                  validation='Please provide a Mobile Number'
+                  label="Mobile Number*"
+                  validation="Please provide a Mobile Number"
                   invalid
                 />
               </Col>
               <Col>
                 <MDBInput
                   value={formValue.company}
-                  name='company'
-                  onChange={onChange}
-                  id='validationCustom05'
-                  label='Company'
+                  name="company"
+                  type="text"
+                  onChange={(e) => handleChange(e)}
+                  id="company"
+                  label="Company"
                 />
               </Col>
             </Row>
             <Row className="mt-4">
               <Col md={1}>
-                <MDBRadio name='inlineRadio' id='inlineRadio1' value='option1' label='Active' inline required invalid/>
+                <MDBRadio
+                  name="inlineRadio"
+                  id="inlineRadio"
+                  value="active"
+                  onChange={(e) => handleChange(e)}
+                  label="Active"
+                  inline
+                  required
+                  invalid
+                />
               </Col>
               <Col>
-                <MDBRadio name='inlineRadio' id='inlineRadio2' value='option2' label='Inactive' inline />
+                <MDBRadio
+                  name="inlineRadio"
+                  id="inlineRadio"
+                  value="inactive"
+                  onChange={(e) => handleChange(e)}
+                  label="Inactive"
+                  inline
+                />
               </Col>
-              <Col md = {1}></Col>
+              <Col md={1}></Col>
               <Col md={7}>
-                
                 <div className="addInvItem">
-                  <label>User Type <sup className="required">*</sup></label>
-                    <select
-                      className="addInvInput"
-                      name="UserType"
-                      onChange={onChange}
-                      value={formValue.type}
-                    >
+                  <label>
+                    User Type <sup className="required">*</sup>
+                  </label>
+                  <select
+                    className="addInvInput"
+                    name="UserType"
+                    id="UserType"
+                    onChange={(e) => handleChange(e)}
+                    value={formValue.UserType}>
                     <option value="">--Select--</option>
                     <option value="Arranger">Arranger</option>
                     <option value="Vendor">Vendor</option>
                     <option value="Anchor">Anchor</option>
                     <option value="Admin">Admin</option>
-                    </select>
+                  </select>
                 </div>
               </Col>
             </Row>
-            
-            </Container>
-      
+          </Container>
         </Modal.Body>
         <Modal.Footer>
-          <Button className="Add" type='submit'>Add</Button>
+          <Button className="Add" type="submit">
+            Add
+          </Button>
         </Modal.Footer>
-        </MDBValidation>
-      </Modal>
-    );
-  }
+      </form>
+      {/* </MDBValidation> */}
+    </Modal>
+  );
+}
